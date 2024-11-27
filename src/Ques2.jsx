317a4159e0d42f8392405584ea7e8b1ee3fa23e3
Sidharth <sunilsidharth411@gmail.com>
2024-11-27 13:09:26 +0530

    final1

--- src/Ques2.jsx
+++ src/Ques2.jsx
@@ -1,15 +1,25 @@
 import React, { useState } from 'react'
 
 function Ques2() {
-    const [name,setName]=useState('')
-    const[colors,setColor]=useState("blue")
+  //   const [name,setName]=useState('')
+  //   const[colors,setColor]=useState("blue")
+  // return (
+  //   <>
+  //   <input type="text" placeholder='Enter name' onChange={(e)=>{setName(e.target.value)}} />
+  //   <button onClick={()=>{setColor({colors:"blue"})}}>chang color</button>
+  //   <div style={{color:colors}}>{name}</div>
+  //   </>
+  // )
+  const [text, setText] = useState('')
+    const [button, setButton] = useState(false)
   return (
     <>
-    <input type="text" placeholder='Enter name' onChange={(e)=>{setName(e.target.value)}} />
-    <button onClick={()=>{setColor({colors:"blue"})}}>chang color</button>
-    <div style={{color:colors}}>{name}</div>
+        <div>Input</div>
+        <input type="text" placeholder='Enter any text' onChange={(e) => setText(e.target.value)}/>
+        <div style={{color: button? 'red':'black'}}>{text}</div>
+        <button onClick={() => {setButton(!button)}}>Change Color</button>
     </>
-  )
+  );
 }
 
 export default Ques2
